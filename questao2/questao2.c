a22fc647a947a45f7ada84c40320bb9958b373e8
AnnyBeatrizsz <annybeatrizscn@gmail.com>
2026-09-20 23:33:12 -0300

    feat: adiciona resolucao da questao 2

--- questao2/questao2.c
+++ questao2/questao2.c
@@ -3,3 +3,23 @@
 // e frequencia minima de 75%.
 
 // Adicione toda a logica em um unico if.
+
+#include <stdio.h>
+
+int main(){
+    char estudante [40];
+    int frequencia;
+    float nota;
+
+    printf("Qual o nome do estudante? ");
+    scanf("%s", estudante);
+    printf("Qual a nota de %s? ", estudante);
+    scanf("%f", &nota);
+    printf("Qual a frequencia de %s? ", estudante);
+    scanf("%d", &frequencia);
+
+    if (nota >= 7.0 && frequencia >= 75)
+        printf("\nO(A) estudante %s foi APROVADO!\n", estudante);
+    else
+        printf("\nO(A) estudante %s foi REPROVADO.\n", estudante);
+}
