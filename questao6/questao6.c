b6a5205478010499f5a54c1e01f0134581c8b197
AnnyBeatrizsz <annybeatrizscn@gmail.com>
2026-09-21 00:37:19 -0300

    feat: adiciona resolucao da questao 6

--- questao6/questao6.c
+++ questao6/questao6.c
@@ -25,3 +25,17 @@ return 0;
 // b) Qual a diferenca entre o while e do do..while?
 // c) Como fica a implementacao do bloco
 // d) Qual a diferenca entre while e for? Quando usar um ou outro?
+
+// a) Vai "printar" o while do 0 ao 9, pois 10 não entraria (10 não é menor que 10).
+//    Depois "printa" o do com o 10, pois primeiro ele executa e depois verifica.
+//    Obs: Caso o i tivesse sido printado depois, ele seria 11.
+
+// b) O while verifica se está dentro da condição e depois executa, já o do executa e somente depois verifica. O do sempre vai executar pelo menos uma vez.
+
+// c) O enunciado parece cortado, mas geralmente pede a reescrita do bloco usando for. Ficaria assim:
+//    for(int i = 0; i < 10; i++) {
+//        printf("for: %d\n", i);
+//    }
+
+// d) O while não tem o contador direto na condição, ao contrário do for. 
+//    O ideal é usar o for quando precisar de contador e o while quando não precisar.
